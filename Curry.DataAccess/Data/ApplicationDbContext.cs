a6afe6d9c2db0117838b11ab1d2df855109dd445
Erika Fermin <erikafermin@mail.weber.edu>
2020-01-17 12:09:54 -0700

    videos 1-6

--- Curry.DataAccess/Data/ApplicationDbContext.cs
+++ Curry.DataAccess/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Curry.Data
+namespace Curry.DataAccess.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
