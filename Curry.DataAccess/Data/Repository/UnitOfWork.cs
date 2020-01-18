2907da691898c4aa7f89f84c99092efe6345a8ed
Erika Fermin <erikafermin@mail.weber.edu>
2020-01-18 16:02:35 -0700

    unitofwork

--- Curry.DataAccess/Data/Repository/UnitOfWork.cs
+++ Curry.DataAccess/Data/Repository/UnitOfWork.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using Curry.DataAccess.Data.Repository.IRepository;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Curry.DataAccess.Data.Repository
 {
-    class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        private readonly ApplicationDbContext _db;
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public ICategoryRepository Category { get; private set; }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
     }
 }
