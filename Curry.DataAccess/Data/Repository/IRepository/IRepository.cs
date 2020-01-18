d7232a3825ef38ed5beb4cf6afb089005e8da4cd
Erika Fermin <erikafermin@mail.weber.edu>
2020-01-17 19:25:41 -0700

    video 8 done

--- Curry.DataAccess/Data/Repository/IRepository/IRepository.cs
+++ Curry.DataAccess/Data/Repository/IRepository/IRepository.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Curry.DataAccess.Data.Repository.IRepository
 {
-    interface IRepository
+    public interface IRepository<T> where T : class
     {
+        //get object by id
+        T Get(int id);
+        // get all objects as Ienumerable
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filer = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null);
+
+        //get the first or default
+        T GeFirstOrDefault(
+          Expression<Func<T, bool>> filer = null,
+          string includeProperties = null);
+
+        //Add
+        void Add(T entity);
+        //Remove(id)
+        void Remove(int id);
+        //Remove(obj)
+        void Remove(T entity);
     }
 }
