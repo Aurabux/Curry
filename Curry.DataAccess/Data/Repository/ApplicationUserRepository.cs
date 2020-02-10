609c5912c05fdc4d013f8b4b1c69d008bab45fcd
Erika Fermin <erikafermin@mail.weber.edu>
2020-02-10 11:12:23 -0700

    Application models added

---
+++ Curry.DataAccess/Data/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Curry.DataAccess.Data.Repository.IRepository;
+using Curry.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Curry.DataAccess.Data.Repository
+{
+    public class ApplicationUserRepository : Repository<Category>, IApplicationUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+    }
+}
--- Curry.DataAccess/Data/Repository/IRepository/IUnitOfWork.cs
+++ Curry.DataAccess/Data/Repository/IRepository/IUnitOfWork.cs
@@ -9,6 +9,7 @@ namespace Curry.DataAccess.Data.Repository.IRepository
        ICategoryRepository Category{ get;}
        IFoodTypeRepository FoodType { get;}
        IMenuItemRepository MenuItem { get; }
+       IApplicationUserRepository ApplicationUser { get; }
         void Save();
     }
 }
