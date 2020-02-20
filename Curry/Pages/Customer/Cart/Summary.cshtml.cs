43f8d49277393ba491bd0e0da009843a44e55119
Erika Fermin <erikafermin@mail.weber.edu>
2020-02-20 14:06:59 -0700

    summarry curry 5

---
+++ Curry/Pages/Customer/Cart/Summary.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Curry.DataAccess.Data.Repository.IRepository;
+using Curry.Models;
+using Curry.Models.ViewModels;
+using Curry.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Curry.Pages.Customer.Cart
+{
+    public class SummaryModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public SummaryModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public OrderDetailsCartVM OrderDetailsCartVM { get; set; }
+        public void OnGet()
+        {
+            OrderDetailsCartVM = new OrderDetailsCartVM()
+            {
+                OrderHeader = new Models.OrderHeader(),
+                listCart = new List<ShoppingCart>()
+            };
+            OrderDetailsCartVM.OrderHeader.OrderTotal = 0;
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            if (claim != null)
+            {
+                IEnumerable<ShoppingCart> cart = _unitOfWork.ShoppingCart.GetAll(c => c.ApplicationUserId == claim.Value);
+
+                if (cart != null)
+                {
+                    OrderDetailsCartVM.listCart = cart.ToList();
+
+                }
+                foreach (var cartList in OrderDetailsCartVM.listCart)
+                {
+                    cartList.MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault
+                        (m => m.Id == cartList.MenuItemId);
+                    OrderDetailsCartVM.OrderHeader.OrderTotal +=
+                        (cartList.MenuItem.Price * cartList.Count);
+                }
+                OrderDetailsCartVM.OrderHeader.OrderTotal +=
+                    OrderDetailsCartVM.OrderHeader.OrderTotal * SD.SalesTaxPercent;
+                ApplicationUser applicationUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(c => c.Id == claim.Value);
+                OrderDetailsCartVM.OrderHeader.DeliveryName = applicationUser.FullName;
+                OrderDetailsCartVM.OrderHeader.DeliveryTime = DateTime.Now;
+                OrderDetailsCartVM.OrderHeader.PhoneNumber = applicationUser.PhoneNumber;
+
+            }
+        }
+    }
+}
